762f41fec5c944aceb1dda43f26fc290a092d826
SJASsEC <33959837+SJASsEC@users.noreply.github.com>
2020-07-09 16:27:46 +0530

    Multiple URLs

--- corshunt.py
+++ corshunt.py
@@ -4,7 +4,7 @@ from requests.exceptions import HTTPError
 def get_urls():    
     file = open(r'urls.txt', "r")
     if file.mode == "r":
-        contents = file.readlines()
+        contents = file.read().splitlines
     return contents
 
 def get_valid_headers():
@@ -23,15 +23,16 @@ def get_valid_headers():
         except HTTPError as http_err:
             print(f'HTTP Error:{http_err}')
         except Exception as err:
-            print(f'Other error:{err}')
+            print(f'Other error in get_valid_headers :{err}')
 
 def advance_hunt():
     same_content= get_urls()
     header_keys = "Access-Control-Allow"
-    headers = {'Origin': 'bing.com'}
-    for url in same_content:
+    header1 = {'Origin': 'bing.com'}
+    for urll in same_content():  
+        # https://stackoverflow.com/questions/30234630/builtin-function-or-method-object-is-not-iterable
         try:
-            response= requests.post(url,headers=headers)
+            response= requests.get(urll,headers=header1)
             if (response.status_code == 200):
                 header = response.headers  
             
@@ -39,23 +40,22 @@ def advance_hunt():
                 filter(lambda item: header_keys in item[0], header.items()))
             # headers containting Access-Control-Allow
             is_empty = not res #if true dictionary is empty
-            final_exploit(is_empty,url)
+            final_exploit(is_empty,urll)
 
-            #print("URL:", url, "Header:", res.keys())
+            #print("URL:", urll, "Header:", res.keys())
         except HTTPError as http_err:
             print(f'HTTP Error:{http_err}')
         except Exception as err:
-            print(f'Other error:{err}')
+            print(f'Other error in advance_hunt:{err}')
 
-def final_exploit(is_empty,url):
+def final_exploit(is_empty,urll):
    # print(is_empty)
     if is_empty:
-        print(url,"-> Not Vulnerable to CORS...")
+        print(urll,"-> Not Vulnerable to CORS...")
     else:
-        print(url,"Vulnerable to CORS")
+        print(urll,"Vulnerable to CORS")
 
 def main():
-    get_urls()
     advance_hunt()
    
 if __name__ == "__main__":
